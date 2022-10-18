fafef28ccea51ffba8a733926fb16563c82efa01
robmartel <99683835+robmartel@users.noreply.github.com>
2022-10-18 15:11:08 -0400

    Featured Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,29 @@ function App() {
       <a href='/'>Welcome To Our Porch</a>
       </header>
       <main>
-        list product
+        <h1>Featured Products</h1>
+        <div className='products'>
+        {
+          data.products.map(product => (
+          <div className='product' key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className='product-info'>
+              <a href={`/product/${product.slug}`}>
+            <p>
+              {product.name}
+            </p>
+            </a>
+            <p><strong>
+              {product.price}
+              </strong>
+            </p>
+            <button>Add to Cart</button>
+            </div>
+          </div>))
+        }
+        </div>
       </main>
     </div>
   );
