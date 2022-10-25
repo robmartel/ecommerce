ca14c41d4b143ada5bf0381fa35a01ffb2033fbf
robmartel <99683835+robmartel@users.noreply.github.com>
2022-10-25 18:27:57 -0400

    Add local storage, remove item add handlers

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -5,15 +5,41 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MessageBox from '../components/MessageBox';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button';
 import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+import Product from '../components/Product';
 
 export default function CartScreen() {
+    const navigate = useNavigate();
   const { state, dispatch: cxtDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    cxtDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+
+  const removeItemHandler = (item) => {
+    cxtDispatch({
+        type: 'CART_REMOVE_ITEM',
+        payload: { ...item}
+    })
+  }
+
+  const checkoutHandler = () => {
+    navigate('/signin?redirect=/shipping');
+  }
 
   return (
     <div>
@@ -43,6 +69,9 @@ export default function CartScreen() {
                     <Col md={3}>
                       <Button
                         variant='light'
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
                         disabled={item.quantity === 1}
                       >
                         <i className='fas fa-minus-circle'></i>
@@ -50,14 +79,19 @@ export default function CartScreen() {
                       <span>{item.quantity}</span>{' '}
                       <Button
                         variant='light'
-                        disabled={item.quantity === 1}
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
+                        disabled={item.quantity === item.countInStock}
                       >
                         <i className='fas fa-plus-circle'></i>
                       </Button>
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant='light'>
+                      <Button 
+                        onClick={() => removeItemHandler(item)}
+                        variant='light'>
                         <i className='fas fa-trash'></i>
                       </Button>
                     </Col>
@@ -72,18 +106,23 @@ export default function CartScreen() {
             <Card.Body>
               <ListGroup variant='flush'>
                 <ListGroup.Item>
-                <h3>
-                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
-                  items) : $
-                  {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
-                </h3>
+                  <h3>
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    items) : $
+                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                  </h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                    <div className='d-grid'>
-                        <Button type='button' variant='primary' disabled={cartItems.length === 0}>
-                            Proceed to Checkout
-                        </Button>
-                    </div>
+                  <div className='d-grid'>
+                    <Button
+                      type='button'
+                      variant='primary'
+                      onClick={checkoutHandler}
+                      disabled={cartItems.length === 0}
+                    >
+                      Proceed to Checkout
+                    </Button>
+                  </div>
                 </ListGroup.Item>
               </ListGroup>
             </Card.Body>
