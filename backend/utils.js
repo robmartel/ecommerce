35322c66209a69df14c8a0e719f1abc015b45c08
robmartel <99683835+robmartel@users.noreply.github.com>
2023-01-25 11:22:33 -0500

    updates

--- backend/utils.js
+++ backend/utils.js
@@ -11,4 +11,22 @@ export const generateToken = (user) => {
         process.env.JWT_SECRET, {
         expiresIn: '30d',
     });
+};
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+        const token = authorization.slice(7, authorization.length); // this will get just the token part, slice 7 because it starts with BEARER XXXXX
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if(err) {
+                res.status(401).send({ message: 'Invalid Token' });
+            } else {
+                req.user = decode;
+                next();
+            }
+        });
+    } else {
+        res.status(401).send({ message: 'No Token' });
+    }
 }
