0b139baadd713b454b06532fbbce762f1019a1d5
robmartel <99683835+robmartel@users.noreply.github.com>
2023-01-26 15:24:48 -0500

    updated profile screen so passwords need to match

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -36,6 +36,10 @@ export default function ProfileScreen() {
 
     const submitHandler = async (e) => {
         e.preventDefault();
+        if(password !== confirmPassword) {
+            toast.error('Passwords do not match');
+            return;
+        }
         try {
             const { data } = await axios.put(
                 '/api/users/profile',
