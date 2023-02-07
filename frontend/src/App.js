bfb98b30e5c3e34ee08f9004444022d9f2f6de50
robmartel <99683835+robmartel@users.noreply.github.com>
2023-02-07 15:51:09 -0500

    added a new api to return all users for admin, created a route in app.js for admin users to user screen, and created a userList screen to show all users to admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,7 @@ import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -257,6 +258,15 @@ function App() {
                 element={
                   <AdminRoute>
                     <OrderListScreen />
+                  </AdminRoute>
+                }
+              />
+
+              <Route
+                path='/admin/users'
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
                   </AdminRoute>
                 }
               />
